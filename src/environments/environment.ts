987963fc60fdd64c5fdb28d5d68ae9f6a34f365e
Judit Santos <judit.sant@gmail.com>
2022-09-25 20:54:23 +0200

    styles updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapboxToken: 'pk.eyJ1IjoianVkaXRzYXR1IiwiYSI6ImNsOGU5MHJlMTA4d2kzdnA5eGFsZDgxbG4ifQ.wMlLCh_YtV97MTyQE_QWQg'
 };
 
 /*
