5fd1bf7375d65bac7f4ef83614210011fbd523de
Judit Santos <judit.sant@gmail.com>
2022-09-28 23:38:34 +0200

    search sitter user + interface

---
+++ src/app/mimos/interfaces/user-sitter.ts
@@ -0,0 +1,12 @@
+export interface UserSitter {
+  profilePicture:   string;
+  name:             string;
+  titleDesc:        string;
+  desc:             string;
+  address:          string;
+  verifiedUser:     boolean;
+  price:            number;
+  dogSitter?:       boolean;
+  catSitter?:       boolean;
+  rating?:          number;
+}
--- src/app/mimos/pages/search-sitter/search-sitter.component.ts
+++ src/app/mimos/pages/search-sitter/search-sitter.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
+import { UserSitter } from '../../interfaces/user-sitter';
+
 @Component({
   selector: 'app-search-sitter',
   templateUrl: './search-sitter.component.html',
@@ -9,6 +11,8 @@ import * as mapboxgl from 'mapbox-gl';
 export class SearchSitterComponent implements OnInit {
 
   constructor() { }
+
+  toggle = true;
 
   ngOnInit(): void {
     var map = new mapboxgl.Map({
@@ -19,5 +23,124 @@ export class SearchSitterComponent implements OnInit {
       scrollZoom: false
     });
   }
+
+  saveFavorite(index: number) {
+    this.toggle = !this.toggle;
+  }
+
+  getClassCSS(): string {
+    return this.toggle ? 'bi bi-heart light-red' : 'bi bi-heart-fill red'
+  }
+
+  userSittersData: UserSitter[] = [
+    {
+      profilePicture:   '/assets/img/gallery/user-card-1.png',
+      name:             'Anna',
+      titleDesc:        'Caring, loving and friendly',
+      desc:             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, dolorum explicabo, quo quos fuga animi expedita voluptatum culpa nisi facere recusandae rem? Vero deleniti distinctio ullam suscipit culpa fugiat est.',
+      address:          'Barcelona, 08002',
+      verifiedUser:     true,
+      price:            15
+    },
+    {
+      profilePicture:   '/assets/img/gallery/user-card-2.png',
+      name:             'Núria',
+      titleDesc:        'Caring, loving and friendly',
+      desc:             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, dolorum explicabo, quo quos fuga animi expedita voluptatum culpa nisi facere recusandae rem? Vero deleniti distinctio ullam suscipit culpa fugiat est.',
+      address:          'Barcelona, 08002',
+      verifiedUser:     false,
+      price:            18
+    },
+    {
+      profilePicture:   '/assets/img/gallery/user-card-3.png',
+      name:             'Fernando',
+      titleDesc:        'Caring, loving and friendly',
+      desc:             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, dolorum explicabo, quo quos fuga animi expedita voluptatum culpa nisi facere recusandae rem? Vero deleniti distinctio ullam suscipit culpa fugiat est.',
+      address:          'Barcelona, 08002',
+      verifiedUser:     true,
+      price:            22
+    },
+    {
+      profilePicture:   '/assets/img/gallery/user-card-1.png',
+      name:             'Sofía',
+      titleDesc:        'Caring, loving and friendly',
+      desc:             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, dolorum explicabo, quo quos fuga animi expedita voluptatum culpa nisi facere recusandae rem? Vero deleniti distinctio ullam suscipit culpa fugiat est.',
+      address:          'Barcelona, 08002',
+      verifiedUser:     true,
+      price:            20
+    },
+    {
+      profilePicture:   '/assets/img/gallery/user-card-2.png',
+      name:             'Raquel',
+      titleDesc:        'Caring, loving and friendly',
+      desc:             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, dolorum explicabo, quo quos fuga animi expedita voluptatum culpa nisi facere recusandae rem? Vero deleniti distinctio ullam suscipit culpa fugiat est.',
+      address:          'Barcelona, 08002',
+      verifiedUser:     true,
+      price:            23
+    },
+    {
+      profilePicture:   '/assets/img/gallery/user-card-3.png',
+      name:             'Albert',
+      titleDesc:        'Caring, loving and friendly',
+      desc:             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, dolorum explicabo, quo quos fuga animi expedita voluptatum culpa nisi facere recusandae rem? Vero deleniti distinctio ullam suscipit culpa fugiat est.',
+      address:          'Barcelona, 08002',
+      verifiedUser:     false,
+      price:            12
+    },
+    {
+      profilePicture:   '/assets/img/gallery/user-card-1.png',
+      name:             'Anna',
+      titleDesc:        'Caring, loving and friendly',
+      desc:             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, dolorum explicabo, quo quos fuga animi expedita voluptatum culpa nisi facere recusandae rem? Vero deleniti distinctio ullam suscipit culpa fugiat est.',
+      address:          'Barcelona, 08002',
+      verifiedUser:     true,
+      price:            30
+    },
+    {
+      profilePicture:   '/assets/img/gallery/user-card-2.png',
+      name:             'Núria',
+      titleDesc:        'Caring, loving and friendly',
+      desc:             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, dolorum explicabo, quo quos fuga animi expedita voluptatum culpa nisi facere recusandae rem? Vero deleniti distinctio ullam suscipit culpa fugiat est.',
+      address:          'Barcelona, 08002',
+      verifiedUser:     false,
+      price:            12
+    },
+    {
+      profilePicture:   '/assets/img/gallery/user-card-3.png',
+      name:             'Fernando',
+      titleDesc:        'Caring, loving and friendly',
+      desc:             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, dolorum explicabo, quo quos fuga animi expedita voluptatum culpa nisi facere recusandae rem? Vero deleniti distinctio ullam suscipit culpa fugiat est.',
+      address:          'Barcelona, 08002',
+      verifiedUser:     true,
+      price:            15
+    },
+    {
+      profilePicture:   '/assets/img/gallery/user-card-1.png',
+      name:             'Sofía',
+      titleDesc:        'Caring, loving and friendly',
+      desc:             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, dolorum explicabo, quo quos fuga animi expedita voluptatum culpa nisi facere recusandae rem? Vero deleniti distinctio ullam suscipit culpa fugiat est.',
+      address:          'Barcelona, 08002',
+      verifiedUser:     true,
+      price:            28
+    },
+    {
+      profilePicture:   '/assets/img/gallery/user-card-2.png',
+      name:             'Raquel',
+      titleDesc:        'Caring, loving and friendly',
+      desc:             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, dolorum explicabo, quo quos fuga animi expedita voluptatum culpa nisi facere recusandae rem? Vero deleniti distinctio ullam suscipit culpa fugiat est.',
+      address:          'Barcelona, 08002',
+      verifiedUser:     true,
+      price:            17
+    },
+    {
+      profilePicture:   '/assets/img/gallery/user-card-3.png',
+      name:             'Albert',
+      titleDesc:        'Caring, loving and friendly',
+      desc:             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, dolorum explicabo, quo quos fuga animi expedita voluptatum culpa nisi facere recusandae rem? Vero deleniti distinctio ullam suscipit culpa fugiat est.',
+      address:          'Barcelona, 08002',
+      verifiedUser:     false,
+      price:            18
+    },
+  ]
 
 }
